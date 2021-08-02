a7c4c6aea3b3723cb361204b23726c9437deefc4
wlademirAntonio <85811422+wlademirAntonio@users.noreply.github.com>
2021-08-02 23:53:03 +0000

    Desenvolvimento da janela modal

---
+++ js/load-page.js
@@ -0,0 +1,12 @@
+//Leitura completa do nosso documento HTML
+$(document).ready(function(){
+
+    //Monitorar todos os cliques em cima do elemento "a" do nosso documento HTML
+
+    $('a').click(function(e){
+        e.preventDefault()
+
+        $('modal-info').modal('show')
+    })
+
+})
